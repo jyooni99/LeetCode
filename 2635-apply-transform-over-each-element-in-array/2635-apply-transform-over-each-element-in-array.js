ba4f467609b8a959b08a9d56a82ed74176109496
JIYOON <112847202+jyooni99@users.noreply.github.com>
2024-10-04 22:09:40 +0900

    Time: 54 ms (40.47%), Space: 48.6 MB (83.52%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,6 +4,10 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let func = fn;
-    return arr.map(func);
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(fn(arr[i], i));
+    }
+
+    return newArr;
 };
